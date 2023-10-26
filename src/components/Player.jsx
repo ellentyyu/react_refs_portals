043cb423d599d114422dca4bd0707c18cdc970cd
ellen <ellentyyu@gmail.com>
2023-10-26 21:22:39 +0800

    feat: 設定玩家名稱邏輯

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [playerName, setPlayerName] = useState(null);
+    const nameInput = useRef();
+    const setNameHandler = () => {
+        setPlayerName(nameInput.current.value);
+        nameInput.current.value = '';
+    }
+
+    return (
+        <section id="player">
+        <h2>Welcome {playerName ?? 'unknown entity'}</h2>
+        <p>
+            <input ref={nameInput} type="text" />
+            <button onClick={setNameHandler}>Set Name</button>
+        </p>
+        </section>
+    );
 }
