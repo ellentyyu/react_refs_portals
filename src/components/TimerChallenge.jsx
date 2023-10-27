ff56b7eb3cd9268b81f7fb4655b9d52192274f68
ellen <ellen_yu@pixnet.tw>
2023-10-27 18:00:28 +0800

    feat: 清除計時器邏輯

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,21 +1,41 @@
+import { useState, useRef } from "react";
 
 const TimerChallenge = ({title, targetTime}) => {
-  return (
-    <section className="challenge">
-        <h2>{title}</h2>
-        <p className="challenge-time">
-            {targetTime} second{targetTime > 1 ? 's' : ''}
-        </p>
-        <p>
-            <button type="button">
-                Start challenge
-            </button>
-        </p>
-        <p>
-            time is running...
-        </p>
-    </section>
-  )
+    const timer = useRef();
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    
+
+    const startChallengeHandler = () => {
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+        }, targetTime * 1000);
+        console.log('set', timer.current);
+        setTimerStarted(true);
+    }
+
+    const stopChallengeHandler = () => {
+        console.log('clear', timer.current);
+        clearTimeout(timer.current);
+    }
+    return (
+        <section className="challenge">
+            <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
+            <p className="challenge-time">
+                {targetTime} second{targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button type="button" onClick={timerStarted ? stopChallengeHandler : startChallengeHandler}>
+                    {timerStarted ? 'Stop' : 'Start'} challenge
+                </button>
+            </p>
+            <p className={timerStarted ? 'active' : null}>
+                {timerStarted ? 'Time is running...' : 'Timer inactive'}
+            </p>
+        </section>
+    )
 }
 
 export default TimerChallenge
