d799fc0a620b443c86dc67982f29ddd19df93314
ellen <ellentyyu@gmail.com>
2023-10-28 22:55:22 +0800

    feat: 使用useImperativeHandle控制彈窗

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,15 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 const ResultModal = forwardRef(({result, targetTime}, ref) => {
+    const dialogModal = useRef();
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialogModal.current.showModal();
+            }
+        }
+    });
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialogModal} className="result-modal">
             <h2>You {result}!</h2>
             <p>your target time was <strong>{targetTime} seconds.</strong></p>
             <p>you stopped the timer with <strong>x seconds left.</strong></p>
